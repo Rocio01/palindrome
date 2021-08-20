24c2a5bceaa1ffc9cfc6059f762ea5250abffb1f
Zulma Rocio <zrmartinezg@gmail.com>
2021-08-20 16:48:26 -0500

    Add a rudimentary palindrome tester

--- bundle.js
+++ bundle.js
@@ -1,6 +1,13 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("zr-palindrome");
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()){
+  alert(`"${phrase.content}" is a palindrome!`)
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`)
+}
 
 },{"zr-palindrome":2}],2:[function(require,module,exports){
  module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 let Phrase = require("zr-palindrome");
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()){
+  alert(`"${phrase.content}" is a palindrome!`)
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`)
+}
