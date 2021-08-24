1348e187846fed5f7a66b40efd9c4f8217c0a36e
Zulma Rocio <zrmartinezg@gmail.com>
2021-08-24 16:05:31 -0500

    Add event listener

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 let Phrase = require("zr-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()){
-  alert(`"${phrase.content}" is a palindrome!`)
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+function palindromeTester (){
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
+  
+  if (phrase.palindrome()){
+    alert(`"${phrase.content}" is a palindrome!`)
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`)
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", () => {
+    palindromeTester();
+  });
+  
+});
+
+
